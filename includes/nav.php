2f0522ec8604cdd64848fc02c02ebe1269651b5f
kentiet <ken.tietkien@gmail.com>
2021-09-02 23:30:24 -0700

    restructure admin folder

--- includes/nav.php
+++ includes/nav.php
@@ -24,10 +24,10 @@
           echo "<li><a href='#'>{$cat_title}</a></li>";
         }
         ?>
-        <!-- <li>
-          <a href="#">About</a>
-        </li>
         <li>
+          <a href="/cms/admin">Admin</a>
+        </li>
+        <!-- <li>
           <a href="#">Services</a>
         </li>
         <li>
